cb864d22407c92bcfc1e198ee31b5ddee948daae
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-23 16:38:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,11 +6,12 @@ require 'pry'
    #    language attributes, 
     #     type, 
      #      type value
+
 def reformat_languages(languages)
   new_hash = {}
-  languages.each |styles, name_of_language|
+  languages.each do |styles, name_of_language|
   binding.pry
-    name_of_language.each |language_attributes|
+    name_of_language.each do |language_attributes|
       new_hash[name_of_language] = language_attributes
 end
 end
