26ddd7f8e61afeace7b2775d4548e6e2f85e841d
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-23 17:24:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -12,7 +12,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |styles, language_hash|
     language_hash.each do |language, type_hash|
-      type_hash.each dp |language_attributes, type|
+      type_hash.each do |language_attributes, type|
     binding.pry
 end
 end
